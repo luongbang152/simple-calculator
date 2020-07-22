daecf7f812c064835d13fbd4151303f236f1ecff
luongbang152 <luongbang152@gmail.com>
2020-07-22 15:37:39 +0700

    Remove unused code, add mathjs

--- app/modules/app/screens/Splash.tsx
+++ app/modules/app/screens/Splash.tsx
@@ -1,26 +1,16 @@
 import React, { useEffect } from 'react';
 import { View } from 'react-native';
-import { useSelector, useDispatch } from 'react-redux';
 
 import R from 'res/R';
-import { ApplicationState } from 'schema';
 import { Navigator } from 'navigation';
 import { openFirstTime } from 'app-redux/actions';
 
 export default function Splash() {
-	const dispatch = useDispatch();
-	const session = useSelector((state: ApplicationState) => state.session);
-
 	useEffect(() => {
-		if (session.firstTimeAt) {
-			setTimeout(() => {
-				Navigator.openApp();
-			}, 500);
-		} else {
-			dispatch(openFirstTime());
-			// other initialization
-		}
-	}, [session.firstTimeAt]);
+		setTimeout(() => {
+			Navigator.openApp();
+		}, 500);
+	}, []);
 
 	return (
 		<View

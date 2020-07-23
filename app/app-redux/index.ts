abf100038040fe38a9b4af2face4ef8dc3c5ffea
luongbang152 <luongbang152@gmail.com>
2020-07-23 13:43:46 +0700

    remove logs

--- app/app-redux/index.ts
+++ app/app-redux/index.ts
@@ -1,12 +1,6 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
 
 import reducers from './reducers';
 
-export const store = __DEV__
-	? createStore(
-			reducers,
-			compose(applyMiddleware(thunk), applyMiddleware(logger)),
-	  )
-	: createStore(reducers, applyMiddleware(thunk));
+export const store = createStore(reducers, applyMiddleware(thunk));
